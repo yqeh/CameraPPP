42a128925d5b01b3616c37e0f5259e3085e56736
Otis Lin <otis@banfiww.com>
2026-05-21 21:53:05 +0800

    Initial Commit

---
+++ CameraPPP/CameraPPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraPPPApp.swift
+//  CameraPPP
+//
+//  Created by Otis Lin on 2026/5/21.
+//
+
+import SwiftUI
+
+@main
+struct CameraPPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CameraPPP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CameraPPP
+//
+//  Created by Otis Lin on 2026/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
